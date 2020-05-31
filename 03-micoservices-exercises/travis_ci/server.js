e9210718f2eafb0a3541b92e9f3ddd8ce8d3ed61
gmpatil <>
2020-05-30 17:36:04 -0700

    feat: Pass environment variables to server.js thru Travis-CI and Docker build.

--- 03-micoservices-exercises/travis_ci/server.js
+++ 03-micoservices-exercises/travis_ci/server.js
@@ -4,9 +4,11 @@ function sleep(ms) {
 
 async function main() {
   const favoriteFood = process.env.FAVORITE_FOOD;  
+  const buildNumber = process.env.BUILD_NUM;  
   
   while(true) {
     console.log(`My favorite food is ${favoriteFood}`);    
+    console.log(`Build #: ${buildNumber}`);    
     console.log('Containers rule!');
     await sleep(5000);
   }
